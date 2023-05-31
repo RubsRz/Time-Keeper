a6c661a426b12a309aa215eed930e7723f339f3e
alvaro-189 <200138@utags.edu.mx>
2023-05-31 16:52:19 -0600

    Modificacion de route y creación de componente de layut

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route ,Redirect} from 'react-router-dom';
 import Login from './components/login/login';
 import Register from './components/register/register';
+import Layout from './components/layout/layout';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        {/* Otras rutas */}
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/login" />
+      </Route>
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/layout" component={Layout} />
+      {/* Otras rutas */}
+    </Switch>
+  </Router>
+    //<Login/>
   );
 };
 
---
+++ src/components/layout/layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import styles from '/layout.module.css';
+function Layout(){
+    return(
+        <div>
+            Hola Layout
+        </div>
+    );
+}
+export default Layout;
