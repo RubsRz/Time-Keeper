f4decda7cf45bf25d50cc082bdc43b61cadc03e1
Beatriz Martinez Perez <trism460@gmail.com>
2023-07-02 14:56:37 -0600

    Add initial design for navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/login/login';
 import Register from './components/register/register';
+import Information from './components/information/information';
 
 const App = () => {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" component={Login} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/information" component={Information} />
         {/* Otras rutas */}
       </Switch>
     </Router>
---
+++ src/components/information/information.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from '../general/navbar';
+
+function Information() {
+  return (
+    <Navbar />
+  );
+}
+
+export default Information;
